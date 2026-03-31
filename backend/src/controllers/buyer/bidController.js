f9a64e422d6789ac929c88ccf709d613f4a8abac
Vinit Bawjee <bawjeeai@gmail.com>
2026-03-31 11:06:37 +0530

    new update

--- backend/src/controllers/buyer/bidController.js
+++ backend/src/controllers/buyer/bidController.js
@@ -1,5 +1,7 @@
 import Auction from "../../models/Auction.js";
 import Product from "../../models/Product.js";
+import sendEmail from "../../utils/sendEmail.js";
+import Buyer from "../../models/Buyer.js";
 
 export const placeBid = async (req, res) => {
   try {
@@ -9,9 +11,9 @@ export const placeBid = async (req, res) => {
 
     if (!product) return res.status(404).json({ message: "Product not found" });
 
-    const highestBid = await Auction.findOne({ product: product._id }).sort({
-      bidAmount: -1,
-    });
+    const highestBid = await Auction.findOne({ product: product._id })
+      .sort({ bidAmount: -1 })
+      .populate("buyer");
 
     const currentBid = highestBid ? highestBid.bidAmount : product.price;
 
@@ -26,6 +28,20 @@ export const placeBid = async (req, res) => {
       buyer: req.user.id,
       bidAmount: amount,
     });
+
+    if (highestBid && highestBid.buyer && highestBid.buyer._id.toString() !== req.user.id) {
+      const subject = "You have been outbid";
+
+      const text = `Hello ${highestBid.buyer.name}, your bid of ${highestBid.bidAmount} has been surpassed by a higher bid of ${amount}.`;
+
+      try {
+        await sendEmail({
+          to: highestBid.buyer.email,
+          subject,
+          text,
+        });
+      } catch {}
+    }
 
     res.status(201).json({
       message: "Bid placed successfully",
